5e34d78e7d5d3e37ea2b69e403807879c0a53384
Jakub Wichniarek <jakubwichniarek@yahoo.pl>
2022-08-20 14:17:51 +0200

    Remove liquibase and db-changelog-master and changing for insert object for rent for insert into database to data.sql

---
+++ src/main/resources/data.sql
@@ -0,0 +1,6 @@
+insert into object_for_rent (name, unit_price, surface, description) values
+    ('Pokój dla studenta', 123.90, 9.1, 'Pokój jednoosobowy dla osoby studiującej. W mieszkaniu jest kuchnia, łazienka i balkon.'),
+    ('Biuro nad bałtykiem', 250.99, 20.0, 'Biuro znajduje się na 5 piętrze z widokiem na bałtyk'),
+    ('Dom jednorodzinny', 1500.00, 79.0, 'Wynajmę dom z ogrodem dla rodziny, możliwość wynajmu długoterminowego'),
+    ('Mieszkanie dwupokojowe', 2500.00, 120.0, 'Mieszkanie dwupokojowe w centrum miasta, chętnie wynajmę dla rodziny z dziećmi'),
+    ('Biuro w centrum Warszawy', 150.0, 10.0, 'Wynajmę biuro w centrum Warszawy z widokiem na Pałac Kultury');
